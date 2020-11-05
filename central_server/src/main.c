ab8edc524106791a53679fa3fc805607f6eefce2
Welison Almeida <welison.almeida.923@gmail.com>
2020-11-04 21:44:58 -0300

    Add threads to central server.

--- central_server/src/main.c
+++ central_server/src/main.c
@@ -1,11 +1,15 @@
 #include "tcp_server.h"
 #include "tcp_client.h"
 #include <signal.h>
+#include <pthread.h>
 
+/* Global variables */
 struct system_data all_system_data;
+pthread_t set_environment_thread;
 
 /* Main functions */
 void handle_all_interruptions(int signal);
+void* post_it();
 
 /*!
  * @brief This function starts execution of the program.
@@ -20,11 +24,19 @@ int main(int argc, char *argv[]) {
     signal(SIGSEGV, handle_all_interruptions);
     signal(SIGPIPE, handle_all_interruptions);
 
-    sleep(2);
+    pthread_create(&set_environment_thread, NULL, &post_it, NULL);
 
     initialize_tcp_server(&all_system_data);
 
     return 0;
+}
+
+void* post_it() {
+    int option = 0;
+    while(1) {
+        scanf("%d", &option);
+        send_data(option);
+    }
 }
 
 /*!
